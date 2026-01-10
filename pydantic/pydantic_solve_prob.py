73df320dda0a5182cdc63541037d21d23b2251f6
Pawan-Chahar <pavankuchahar@gmail.com>
2026-01-10 16:15:46 +0530

    data validation

--- pydantic/pydantic_solve_prob.py
+++ pydantic/pydantic_solve_prob.py
@@ -1,15 +1,15 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List ,Dict, Optional
+from typing import List ,Dict, Optional, Annotated
 
 class Patient(BaseModel):
 
-    name: str 
+    name: str = Field(max_length= 50 , )
     email: EmailStr
     linkedin_url: AnyUrl
     age: int = Field(gt=0, lt=120)
     weight: float = Field(gt=0)
     married: bool=False
-    allergies: Optional[List[str]] = None
+    allergies: Optional[List[str]] = Field(max_length=5)
     contact_details: Dict[str, str]
 
 # def insert_patient_data(patient: Patient):
@@ -32,7 +32,7 @@ def update_patient_data(patient: Patient):
     print('Update into Database')
 
 
-patient_info = {'name': 'mohan', 'email' : 'abc@gmail.com' ,'linkedin_url':'http://linkdin.com/14576', 'age': 30, 'weight': 89.17 ,'contact_details': {'email' : 'abc@gmail.com','phone': '2353462'}}
+patient_info = {'name': 'mohan shyam', 'email' : 'abc@gmail.com' ,'linkedin_url':'http://linkdin.com/14576', 'age': 30, 'weight': 89.17 ,'contact_details': {'email' : 'abc@gmail.com','phone': '2353462'}}
 
 patient1 = Patient(**patient_info)
 
