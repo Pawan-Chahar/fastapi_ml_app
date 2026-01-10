4884160a5cf63aec1ef6b80fa447c8acc44b13da
Pawan-Chahar <pavankuchahar@gmail.com>
2026-01-10 14:11:41 +0530

    add pydantic why

---
+++ pydantic/pydantic_why.py
@@ -0,0 +1,23 @@
+
+def insert_patient_data(name: str,age: int):
+
+    if type(name) == str and type(age) == int:
+        if age<0:
+            raise ValueError('Age cant be negative')
+        else:
+            print(name)
+            print(age)
+            print('Inserted into Database')
+    else:
+        raise TypeError('Incorrect data type')
+    
+
+def update_patient_data(name:str , age: int):
+    if type(name) == str and type(age) == int:
+        print(name)
+        print(age)
+        print('Inserted into Database')
+    else:
+        raise TypeError('Incorrect data type') 
+
+insert_patient_data('mohan','30')
