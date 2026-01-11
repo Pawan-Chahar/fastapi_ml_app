7df0d995960e311078658703a98ff3f9443da676
Pawan-Chahar <pavankuchahar@gmail.com>
2026-01-11 13:11:58 +0530

    seriallization added

---
+++ pydantic/serialization.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+
+class Patient(BaseModel):
+
+    name:str
+    gender: str
+    age: int 
+    address: Address
+
+
+address_dict = {'city': 'gurgaon' , 'state': 'haryana', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'mohan', 'gender': 'male', 'age': 35 , 'address': address1}
+
+patient1 = Patient(**patient_dict)
+print(patient1)
+print(patient1.name)
+print(patient1.address.city)
+print(patient1.address.pin)
+
+temp=patient1.model_dump()
+print(temp)
+
+print(type(temp))
+
+temp=patient1.model_dump_json()
+print(temp)
+
+print(type(temp))
+
+
+temp=patient1.model_dump(include={'name','age'})
+print(temp)
+
+print(type(temp))
+
+temp=patient1.model_dump(exclude={'age' , 'gender'})
+print(temp)
+
+print(type(temp))
+
+temp=patient1.model_dump(exclude={'address':['state']})
+print(temp)
+
+print(type(temp))
